9718b46c6fa97195cc45572750bd5bdf4cb5b934
KaneUng <jwadeung98@gmail.com>
2026-05-15 14:48:41 +0900

    gtest 초기 환경 세팅 -2

---
+++ build/TVControllerTest[1]_tests.cmake
@@ -0,0 +1,3 @@
+add_test([=[TVControllerTest.testFramework]=]  C:/DEV/TDD_TV_Init/build/TVControllerTest.exe [==[--gtest_filter=TVControllerTest.testFramework]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[TVControllerTest.testFramework]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV\TDD_TV_Init\test\TVControllerTest.cpp:13]==] WORKING_DIRECTORY C:/DEV/TDD_TV_Init/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  TVControllerTest_TESTS TVControllerTest.testFramework)
